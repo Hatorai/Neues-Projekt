a3fb394fec9335184af0146b6443e4b5677e36ac
MaksNohr <m.nohr@freenet.de>
2024-09-20 09:35:29 +0200

    Kalender funktioniert mit vor und zurück

--- Projekt QESTIT/src/script.js
+++ Projekt QESTIT/src/script.js
@@ -1,7 +1,7 @@
 const calendarBody = document.getElementById('calendar-body');
 const today = new Date();
-const month = today.getMonth();
-const year = today.getFullYear();
+let month = today.getMonth();
+let year = today.getFullYear();
 const day = today.getDate();
 
 // Funktion, um den Kalender zu generieren
@@ -9,6 +9,8 @@ function generateCalendar(currentDay) {
   const firstDayOfMonth = new Date(year, month, 1);
   const lastDayOfMonth = new Date(year, month + 1, 0);
   const daysInMonth = lastDayOfMonth.getDate();
+
+
 
   let row = '';
   let dayCounter = 1;
@@ -25,8 +27,8 @@ function generateCalendar(currentDay) {
       if (i === 0 && j < startDay) {
         row += '<td></td>'; // leere Zelle für Tage vor dem ersten Tag des Monats
       } else if (dayCounter <= daysInMonth) {
-        if (dayCounter === currentDay) {
-          row += `<td class="current-date">${dayCounter}</td>`; // heutigen Tag hervorheben
+        if (dayCounter === currentDay ) {
+          row += `<td>${dayCounter}</td>`; // heutigen Tag hervorheben
         } else {
           row += `<td>${dayCounter}</td>`;
         }
@@ -42,9 +44,36 @@ function generateCalendar(currentDay) {
   calendarBody.innerHTML = row;
 }
 
+
 // Bestimmen Sie den aktuellen Tag extern
 const currentDayOfWeek = new Date().getDay();
 const currentDayOfMonth = new Date().getDate();
 
-// Generieren Sie den Kalender mit dem aktuellen Tag
+// Generieren Sie den Kalender
 generateCalendar(currentDayOfMonth);
+
+//Funktion zum steuern des Kalenders
+const prevButton = document.getElementById('prev-button');
+const nextButton = document.getElementById('next-button');
+
+prevButton.addEventListener('click', () => {
+  month--;
+  if (month < 0) {
+    month = 11;
+    year--;
+  }
+  generateCalendar(1); // Generate calendar for the first day of the month
+});
+
+// Event Listener für den  next Button
+nextButton.addEventListener('click', () => {
+    month++;
+    if (month > 11) {
+        month = 0;
+        year++;
+    }
+    generateCalendar(1); // Kalender für den ersten Tag des Monats generieren
+});
+
+// Initialer Aufruf zur Generierung des Kalenders
+generateCalendar(1);
